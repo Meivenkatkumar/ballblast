c8a1879af744a64651b705e3e59eb18d1b8e9935
Meivenkatkumar <ln.meivenkatkumar21@gmail.com>
2019-06-10 17:37:17 +0530

    final done

--- canon.js
+++ canon.js
@@ -2,9 +2,9 @@ var canvas=document.querySelector('canvas');
 canvas.width=1000;
 canvas.height=800;
 var c = canvas.getContext('2d');
-var roc=new Image();
-roc.src="4.jpg";
-var i,count=0,mode=3;
+var bg=new Image();
+bg.src="1.jpg";
+var i,count=0,mode,pre=11,turn=0;
 var highscore=0;
 if (localStorage.hasOwnProperty("bbhs")) {
     highscore=parseInt(localStorage.getItem("bbhs"));
@@ -25,26 +25,43 @@ class canon{
     this.score=0;
     }
     build(){
-    c.fillRect(this.x+10,this.y,40,40);
+    c.fillStyle='rgb(41, 45, 51)';
+    c.fillRect(this.x+10,this.y,40,20);
     c.beginPath();
-    c.fillStyle='rgb(0,255,0)';
+    c.arc(this.x+30,this.y+20,20,0,Math.PI);
+    c.fill();
+    c.stroke();
+    c.beginPath();
+    c.fillStyle='rgb(160,82,45)';
+    c.strokeStyle='rgb(160,82,45)';
     c.arc(this.x+10,this.y+30,10,0,Math.PI*2);
     c.fill();
     c.stroke();
     c.beginPath();
+    c.fillStyle='rgb(139,69,19)';
+    c.arc(this.x+10,this.y+30,4,0,Math.PI*2);
+    c.fill();
+    c.stroke();
+    c.beginPath();
+    c.fillStyle='rgb(160,82,45)';
     c.arc(this.x+50,this.y+30,10,0,Math.PI*2);
     c.fill();
     c.stroke();
-    c.fillStyle='rgb(255,0,0)';
+    c.beginPath();
+    c.fillStyle='rgb(139,69,19)';
+    c.arc(this.x+50,this.y+30,4,0,Math.PI*2);
+    c.fill();
+    c.stroke();
+
     }
     right(){
-    if(this.x<940)
+    if((this.x<940)&&(count>=100))
      {
         this.x=this.x+this.dx;
      }
     }
     left(){
-    if(this.x>0)
+    if((this.x>0)&&(count>=100))
      {
         this.x=this.x-this.dx;
      }
@@ -53,32 +70,32 @@ class canon{
 class bullet{
     constructor(i){
         this.x=can.x+30;
-        this.y=800+(i*25);
+        this.y=750+(i*30);
         this.r=10;
-        this.dy=15;
+        this.dy=10;
         this.display=1;
         this.d1=0;
         this.d2=0;
     }
     initialise(){
         this.x=can.x+30;
-        this.y=can.y;
+        this.y=750;
         this.display=1;
     }
     restart(i){
         this.x=can.x+30;
-        this.y=800+(i*25);
+        this.y=750+(i*30);
         this.r=10;
-        this.dy=15;
+        this.dy=10;
         this.display=1;
         this.d1=0;
         this.d2=0;
     }
     build(){
-        if(this.y<0)
+        if(this.y<=0)
             this.initialise();
         this.moveup();
-        if(this.display)
+        if((this.display)&&(this.y<=750))
         {
         c.beginPath();
         c.arc(this.x,this.y,this.r,0,Math.PI*2);
@@ -264,10 +281,6 @@ class rock{
         {  
            if(this.strength>100)
            {
-              // c.save();
-              // c.rotate(Math.PI);
-              // c.drawImage(roc,0,0,300,300,this.x-this.r,this.y-this.r,2*this.r,2*this.r);
-              // c.restore();
               c.beginPath();
               c.arc(this.x,this.y,this.r,0,Math.PI*2);
               c.fillStyle='rgb(255,255,0)';
@@ -409,17 +422,19 @@ class rock{
     }
     score(){
         if(this.strength>100)
-        {
+        {   
+        	c.font = "normal 26px Verdana";
             c.fillStyle='rgb(255,0,0)';
-            c.fillText(this.strength,this.x,this.y);
+            c.fillText(this.strength,this.x-20,this.y+5);
         }
         else if(this.strength<=100)
         {
+        	c.font = "normal 10px Verdana";
             c.fillStyle='rgb(255,0,0)';
             if(this.strength1>0)
-              c.fillText(this.strength1,this.x1,this.y);
+              c.fillText(this.strength1,this.x1-7,this.y);
             if(this.strength2>0)
-              c.fillText(this.strength2,this.x2,this.y);
+              c.fillText(this.strength2,this.x2-7,this.y);
         }
     }
 }
@@ -431,9 +446,9 @@ function allrestart(){
             rocks[i].initial();
             rocks[i].timer=0;
           }
-          for(i=0;i<=31;i++)
+          for(i=0;i<25;i++)
           {
-          bulls[i].restart();
+          bulls[i].restart(i);
           }
 }
 function move(event){
@@ -445,21 +460,45 @@ function move(event){
     {
         can.right();
     }
+    if(event.keyCode==65)
+    {
+        can.left();
+    }
+    else if(event.keyCode==68)
+    {
+        can.right();
+    }
+    // if(event.keyCode==32)
+    // { 
+    // 	if(turn%2==0)
+    // 	{
+    // 	turn+=1;
+    // 	clearInterval(reset);
+    //     } 
+    //     else 
+    //     {
+    //     	turn+=1;
+    //     	reset=setInterval(animate,20);
+    //     }
+    // }
 }
 var can =new canon();
 var bulls=[];
 var rocks=[];
-for(i=0;i<mode;i++)
+for(i=0;i<5;i++)
   rocks.push(new rock());
-for(i=0;i<=31;i++)
+for(i=0;i<25;i++)
   bulls.push(new bullet(i));
 window.addEventListener("keydown",move,true);
 function animate(){
-	c.clearRect(0,0,window.innerWidth,window.innerHeight); 
+  c.clearRect(0,0,window.innerWidth,window.innerHeight); 
+  c.drawImage(bg,0,0,1279,625,0,0,1000,800);
+  c.font="normal 20px Verdana";
   c.fillStyle='rgb(255,0,0)';
   c.fillStyle='rgb(255,0,0)';
   c.fillText(can.score,500,40);
-  c.fillText(highscore,600,40);
+  c.fillText("HIGHSCORE",600,20);
+  c.fillText(highscore,640,40);
   can.build();
   for(i=0;i<mode;i++)
     {
@@ -468,7 +507,10 @@ function animate(){
         rocks[i].build();
       }
     }
-  for(i=0;i<=31;i++)
+    if((can.score%20==0))
+    	for(i=0;i<25;i++)
+    		bulls[i].dy=10+Math.floor(can.score/100); //velocity of bullet increase for every 100 points
+  for(i=0;i<25;i++)
     {
       bulls[i].build();
       for(j=0;j<mode;++j)
@@ -482,5 +524,24 @@ function animate(){
     localStorage.setItem("bbhs",highscore);
   }
   count++;
+  var gradient = c.createLinearGradient(0, 0, 170, 0);
+    gradient.addColorStop("0", "blue");
+    gradient.addColorStop("0.5" ,"magenta");
+    gradient.addColorStop("1.0", "red");
+    c.strokeStyle = gradient;
+    c.strokeRect(1, 1,998,798);
 } 
-setInterval(animate,20);
+function play(){
+document.getElementById('game').style.display = "block";
+document.getElementById('cover').style.display = "none";
+var reset=setInterval(animate,20);
+}
+function set1(){
+	window.mode=1;
+}
+function set2(){
+	window.mode=2;
+}
+function set3(){
+	window.mode=3;
+}
